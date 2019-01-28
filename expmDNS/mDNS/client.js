e4d0116190209fa3af41b0e72e5081956c46c88e
wongwong <luckboystudent@gmail.com>
2019-01-28 13:58:10 +0800

    unit test

---
+++ expmDNS/mDNS/client.js
@@ -0,0 +1,46 @@
+const mdns=require('../node_modules/multicast-dns')();
+let queryque=new Set();
+mdns.on('response',function (res){
+	console.log(res.answers);
+	let promise=new Promise(function(resolve,reject){
+		let arr=[];
+		if(res.answers.map(function(answer){return answer.type}).includes('SRV')){
+			for(let i=0; i < res.answers.length ; i++){
+				if(res.answers[i].type=='SRV' && queryque.has(res.answers[i].name)){
+					mdns.query([{name:res.answers[i].data.target, type:'A'}]);
+					console.log({name:res.answers[i].data.target, type:'A'});
+					queryque.delete(res.answers[i].name);
+				} 
+			}
+		}
+		if(res.answers.map(function(answer){return answer.type}).includes('PTR')){
+			for(let i=0; i < res.answers.length ; i++){
+				if(res.answers[i].type=='PTR'){
+					arr.push(res.answers[i].data);
+					arr.push('PTR');
+					arr.push('SRV');
+					arr.push('TXT');
+					arr.push('A');
+					resolve(arr);
+				}
+			}
+		}
+		
+		
+	});
+	promise.then(function(full){
+		let name=full[0];
+		for(let i = 1 ; i< full.length ; i++){
+			if(full[i]=='SRV'){
+				queryque.add(name);
+				console.log({name:name, type:'SRV'});
+				mdns.query([{name:name, type:'SRV'}]);
+			}
+			if(full[i]=='TXT'){
+				mdns.query([{name:name, type:'TXT'}]);
+				console.log({name:name, type:'TXT'});
+			}	
+		}
+	});
+});
+mdns.query([{name:'_transfer._sub._ssh._tcp.local', type:'PTR'}]);
