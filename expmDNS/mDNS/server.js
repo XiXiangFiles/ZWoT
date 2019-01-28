ec81e6be1df422cc8c633fd208fc9c4a65a66544
wongwong <luckboystudent@gmail.com>
2019-01-28 13:47:26 +0800

    v0.1.0

--- expmDNS/mDNS/server.js
+++ expmDNS/mDNS/server.js
@@ -14,14 +14,17 @@ for(let i = 0 ; i< Object.keys(config.WoTs).length ; i++){
 		let serviceSyntax=`${name}._sub`;
 		for(let j = 0 ; j<service.protocols.length;j++){
 			serviceSyntax+=`._${service.protocols[j]}`;
+			if(j==service.protocols.length-1)
+				serviceSyntax+=".local";
 		}
-		listServicewithIns.push(`${config.Instance}.${serviceSyntax}.local`);
-		srv.set(`${config.Instance}.${serviceSyntax}.local`,JSON.stringify(service.SRV));
-		txt.set(`${config.Instance}.${serviceSyntax}.local`,JSON.stringify(service.TXT));
+		listServicewithIns.push(`${config.Instance}.${serviceSyntax}`);
+		srv.set(`${config.Instance}.${serviceSyntax}`,JSON.stringify(service.SRV));
+		txt.set(`${config.Instance}.${serviceSyntax}`,JSON.stringify(service.TXT));
 		listService.push(serviceSyntax);
 	}
 }
-console.log(listServicewithIns);
+console.log(listService);
+// console.log(listServicewithIns);
 mdns.on('query',function(res){
 	let promise=new Promise(function(resolve,reject){
 		let respond={};
@@ -38,21 +41,21 @@ mdns.on('query',function(res){
 					obj.data=config.Instance+'.'+listService[i];
 					answers.push(obj);
 				}
-			}else if(listService.includes(res.questions.map(function(element){if(element.type=='PTR'){return element.name}}))){
+			}else if(listService.includes(res.questions.map(function(element){if(element.type=='PTR'){return element.name}}).toString())){
 				let obj={};
-				obj.name=res.questions.map(function(element){if(element.type=='PTR'){return element.name}});
+				obj.name=res.questions.map(function(element){if(element.type=='PTR'){return element.name}}).toString();
 				obj.type='PTR';
 				obj.ttl=120;
-				obj.data=config.Instance+'.'+obj.name+".local";
+				obj.data=config.Instance+'.'+obj.name;
 				answers.push(obj);
 			}
 		}
 		if(res.questions.map((element)=>{return element.type}).includes('SRV')){
 			console.log(res.questions.map(function(element){if(element.type=='SRV'){return element.name}}));
-			if(listServicewithIns.includes(res.questions.map(function(element){if(element.type=='SRV'){return element.name}}))){
+			if(listServicewithIns.includes(res.questions.map(function(element){if(element.type=='SRV'){return element.name}}).toString())){
 				let obj={};
-				let serviceSRV=JSON.parse(srv.get(res.questions.map(function(element){if(element.type=='SRV'){return element.name}})));
-				obj.name=res.questions.map(function(element){if(element.type=='SRV'){return element.name}});
+				let serviceSRV=JSON.parse(srv.get(res.questions.map(function(element){if(element.type=='SRV'){return element.name}}).toString()));
+				obj.name=res.questions.map(function(element){if(element.type=='SRV'){return element.name}}).toString();
 				obj.type='SRV';
 				obj.ttl=120;
 				obj.data={};
@@ -64,13 +67,13 @@ mdns.on('query',function(res){
 			}
 		}
 		if(res.questions.map((element)=>{return element.type}).includes('TXT')){
-			if(listServicewithIns.includes(res.questions.map(function(element){if(element.type=='TXT'){return element.name}}))){
+			if(listServicewithIns.includes(res.questions.map(function(element){if(element.type=='TXT'){return element.name}}).toString())){
 				let obj={};
-				let serviceTXT=JSON.parse(txt.get(res.questions.map(function(element){if(element.type=='TXT'){return element.name}})));
-				obj.name=res.questions.map(function(element){if(element.type=='TXT'){return element.name}});
+				let serviceTXT=JSON.parse(txt.get(res.questions.map(function(element){if(element.type=='TXT'){return element.name}}).toString()));
+				obj.name=res.questions.map(function(element){if(element.type=='TXT'){return element.name}}).toString();
 				obj.type='TXT';
 				obj.ttl=120;
-				obj.data={};
+				obj.data=[];
 				for(let i = 0 ; i<serviceTXT.length ;i++)
 					obj.data.push(Buffer.from(serviceTXT[i],'ascii'));
 
