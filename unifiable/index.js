219bda588211669630355e13bd0c5afd5ab85435
wongwong <luckboystudent@gmail.com>
2019-04-12 15:23:07 +0800

    add unifiable and revise the config 1. complete unifiable expression parseValue 2. Service root should have status and values field

--- unifiable/index.js
+++ unifiable/index.js
@@ -1,6 +1,6 @@
 'use strict'
 const unifiable = require('./unfilable.js')
-const color = require('../node_modules/colors')
+const o = require('../get-objvalues')
 function Unifiable () {
   this.encode = function (expression) {
     let answers = ''
@@ -23,6 +23,44 @@ function Unifiable () {
     return answers.trim()
   }
   this.parseValue = function (expression, value) {
+    function parse (object, char) {
+      const res = []
+      for (let i = 0; i < Object.keys(object).length; i++) {
+        if (char === 'key') {
+          res.push(Object.keys(object)[i])
+        } else {
+          const ans = o.get(object[Object.keys(object)[i]], `x.${char}`)
+          if (ans) {
+            res.push(ans)
+          }
+        }
+      }
+      return res
+    }
+    function putData (arr, data) {
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === undefined) {
+          arr[i] = ''
+        }
+        if (data[1].length === 0) {
+          arr[i] += data[0] + ' '
+        } else {
+          arr[i] += data[1][i % data[1].length] + ' '
+        }
+      }
+    }
+    const split = expression.split(' ')
+    const ans = new Map()
+    for (let i = 0; i < split.length; i++) {
+      const pasrseRes = parse(value, split[i])
+      ans.set(split[i], pasrseRes)
+    }
+    let interatre = ans.entries()
+    const finalans = new Array(Object.keys(value).length)
+    for (let j = 0; j < ans.size; j++) {
+      putData(finalans, interatre.next().value)
+    }
+    return finalans
   }
 }
 module.exports = new Unifiable()
