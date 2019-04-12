91b211da1c7b4db5c014a4c28725c9527282e9ce
wongwong <luckboystudent@gmail.com>
2019-04-12 12:59:43 +0800

    v.1.0

---
+++ get-objvalues/index.js
@@ -0,0 +1,14 @@
+exports.get = function (object, exp) {
+  if (typeof (exp) === 'string') {
+    try {
+      const theKey = exp.split(/\.|\['?"?[a-zA-Z1-9]*'?"?\]/)[0]
+      // eslint-disable-next-line no-eval
+      return eval(exp.replace(`${theKey}`, 'object'))
+    } catch (err) {
+      console.error(err)
+      return undefined
+    }
+  } else {
+    return undefined
+  }
+}
