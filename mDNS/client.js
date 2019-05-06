b40305061965c577e3443e8a95d1cde9b868224d
wongwong <luckboystudent@gmail.com>
2019-05-06 11:22:11 +0800

    revised the experiments

--- mDNS/client.js
+++ mDNS/client.js
@@ -10,34 +10,31 @@ if (filename) {
   console.log('please input filename')
   process.exit()
 }
-let count = 0
 async function saveLog () {
-  setInterval(function () {
-    pidusage(process.pid, function (_err, stats) {
-      try {
-        fs.appendFileSync(`${filename}.csv`, `${stats.cpu},${stats.memory}\n`)
-      } catch (e) {
-        fs.writeFileSync(`${filename}.csv`, `${stats.cpu},${stats.memory}`)
-      }
-    })
-  }, 1)
+  pidusage(process.pid, function (_err, stats) {
+    try {
+      console.log(`${filename}.csv`, `${stats.cpu},${stats.memory}\n`)
+      fs.appendFileSync(`${filename}.csv`, `${stats.cpu},${stats.memory}\n`)
+    } catch (e) {
+      fs.writeFileSync(`${filename}.csv`, `${stats.cpu},${stats.memory}`)
+      console.log(`${filename}.csv`, `${stats.cpu},${stats.memory}\n`)
+    }
+  })
+  setTimeout(() => {}, 100)
 }
-let ptrcount = 0
-let ptrcompare = 0
 let dnssdQ = []
 mDNS.on('response', function (packet) {
   dnssdQ = []
+  
   const ptr = packet.answers.filter((e) => { if (e.type === 'PTR') { return e } })
   const srv = packet.answers.filter((e) => { if (e.type === 'SRV') { return e } })
   if (ptr) {
     for (let i = 0; i < ptr.length; i++) {
       if (ptr[i].name === '_services._dns-sd._udp.local') {
         dnssdQ.push({ name: ptr[i].data.toString('utf8'), type: 'PTR', QU: false })
-        ptrcount++
       } else {
         dnssdQ.push({ name: ptr[i].data.toString('utf8'), type: 'SRV', QU: false })
         dnssdQ.push({ name: ptr[i].data.toString('utf8'), type: 'TXT', QU: false })
-        ptrcount++
       }
     }
   }
@@ -48,21 +45,11 @@ mDNS.on('response', function (packet) {
   }
   if (dnssdQ.length > 0) {
     mDNS.query(dnssdQ)
-    ptrcompare += ptr.length
-    console.log(color.red(ptrcount))
-    console.log(color.red(ptrcompare))
-    if (ptrcount === ptrcompare) {
-      console.log(dnssdQ)
-      mDNS.query(dnssdQ)
-      ptrcount = 0
-      ptrcompare = 0
-      console.log(count)
-      if (count++ === 15) {
-        setTimeout(() => { process.exit() }, 1000)
-      }
-    }
+    // console.log(dnssdQ)
+    setTimeout(() => { process.exit() }, 2500)
   }
+  saveLog()
 })
+saveLog()
 dnssdQ.push({ name: '_services._dns-sd._udp.local', type: 'PTR', QU: false })
 mDNS.query(dnssdQ)
-saveLog()
