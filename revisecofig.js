9d1ef79f0e481b073d239ea5b6bb714422c773f7
wongwong <luckboystudent@gmail.com>
2019-05-06 14:36:29 +0800

    v0.0.2

--- revisecofig.js
+++ revisecofig.js
@@ -2,12 +2,18 @@ const fs = require('fs')
 const now = require('date-now')
 const config = JSON.parse(fs.readFileSync('config.json', 'utf8'))
 const newInstance = config.Instance + now()
+const oldInstance = config.Instance
 config.Instance = newInstance
 config.A.name = newInstance + '.local'
 for (let i = 0; i < Object.keys(config.WoTs).length; i++) {
   try {
     config.WoTs[Object.keys(config.WoTs)[i]].SRV.target = config.A.name
+    if (oldInstance === Object.keys(config.WoTs)[i]) {
+      config.WoTs[config.Instance] = config.WoTs[Object.keys(config.WoTs)[i]]
+      delete config.WoTs[oldInstance]
+    }
   } catch (e) {
   }
 }
+console.log(JSON.stringify(config))
 fs.writeFileSync('config.json', JSON.stringify(config))
