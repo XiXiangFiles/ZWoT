b9b092f993c05a04a1f152b1e3c80cfc9a2a99ff
wongwong <luckboystudent@gmail.com>
2019-05-15 15:36:29 +0800

    experiment 2

--- revisecofig.js
+++ revisecofig.js
@@ -1,19 +1,23 @@
-const fs = require('fs')
-const now = require('date-now')
-const config = JSON.parse(fs.readFileSync('config.json', 'utf8'))
-const newInstance = config.Instance + now()
-const oldInstance = config.Instance
-config.Instance = newInstance
-config.A.name = newInstance + '.local'
-for (let i = 0; i < Object.keys(config.WoTs).length; i++) {
-  try {
-    config.WoTs[Object.keys(config.WoTs)[i]].SRV.target = config.A.name
-    if (oldInstance === Object.keys(config.WoTs)[i]) {
-      config.WoTs[config.Instance] = config.WoTs[Object.keys(config.WoTs)[i]]
+
+const request = require('request')
+request(`http://172.17.0.1:3000/zwot`, function (_error, response, body) { 
+  const fs = require('fs')
+  const now = require('date-now')
+  const config = JSON.parse(body)
+  const newInstance = config.Instance + now()
+  const oldInstance = config.Instance
+  config.Instance = newInstance
+  config.A.name = newInstance + '.local'
+  for (let i = 0; i < Object.keys(config.WoTs).length; i++) {
+    try {
+      config.WoTs[Object.keys(config.WoTs)[i]].SRV.target = config.A.name
+      if (oldInstance === Object.keys(config.WoTs)[i]) {
+        config.WoTs[config.Instance] = config.WoTs[Object.keys(config.WoTs)[i]]
+      }
+    } catch (e) {
     }
-  } catch (e) {
   }
-}
-delete config.WoTs[oldInstance]
-console.log(JSON.stringify(config))
-fs.writeFileSync('config.json', JSON.stringify(config))
+  delete config.WoTs[oldInstance]
+  console.log(JSON.stringify(config))
+  fs.writeFileSync('config.json', JSON.stringify(config))
+})
