06db6877c12f4a0a53ce09c684acb3660ac82b15
wongwong <luckboystudent@gmail.com>
2019-05-16 20:05:48 +0800

    add mdnsqos code

--- mDNS/qosmdns.js
+++ mDNS/qosmdns.js
@@ -5,12 +5,13 @@ const fs = require('fs')
 const color = require('../node_modules/colors')
 const now = require('date-now')
 const request = require('../node_modules/request')
+const unfi = require('../unifiable')
 const filename = process.argv[2]
 if (filename) {
 
 } else {
-  console.log('please input filename')
-  process.exit()
+  // console.log('please input filename')
+  // process.exit()
 }
 async function saveLog () {
   pidusage(process.pid, function (_err, stats) {
@@ -34,25 +35,26 @@ mDNS.on('response', function (packet) {
   const ptr = packet.answers.filter((e) => { if (e.type === 'PTR') { return e } })
   const srv = packet.answers.filter((e) => { if (e.type === 'SRV') { return e } })
   const txt = packet.answers.filter((e) => { if (e.type === 'TXT') { return e } })
-  if (txt) {
-    count.push(txt[0])
+  if (ptr.length > 0) {
+    for (let i = 0; i < ptr.length; i++) {
+      count.push(ptr[0].data)
+    }
   }
-  count++
   if (ptr) {
     for (let i = 0; i < ptr.length; i++) {
       if (ptr[i].name === '_services._dns-sd._udp.local') {
         if (!set.has(JSON.stringify({ name: ptr[i].data.toString('utf8'), type: 'PTR', QU: false }))) {
           set.add(JSON.stringify({ name: ptr[i].data.toString('utf8'), type: 'PTR', QU: false }))
-        //   dnssdQ.push({ name: ptr[i].data.toString('utf8'), type: 'PTR', QU: false })
+          dnssdQ.push({ name: ptr[i].data.toString('utf8'), type: 'PTR', QU: false })
         }
       } else {
         if (!set.has(JSON.stringify({ name: ptr[i].data.toString('utf8'), type: 'SRV', QU: false }))) {
           set.add(JSON.stringify({ name: ptr[i].data.toString('utf8'), type: 'SRV', QU: false }))
-        //   dnssdQ.push({ name: ptr[i].data.toString('utf8'), type: 'SRV', QU: false })
+          dnssdQ.push({ name: ptr[i].data.toString('utf8'), type: 'SRV', QU: false })
         }
         if (!set.has(JSON.stringify({ name: ptr[i].data.toString('utf8'), type: 'TXT', QU: false }))) {
           set.add(JSON.stringify({ name: ptr[i].data.toString('utf8'), type: 'TXT', QU: false }))
-        //   dnssdQ.push({ name: ptr[i].data.toString('utf8'), type: 'TXT', QU: false })
+          dnssdQ.push({ name: ptr[i].data.toString('utf8'), type: 'TXT', QU: false })
         }
       }
     }
@@ -61,7 +63,7 @@ mDNS.on('response', function (packet) {
     for (let i = 0; i < srv.length; i++) {
       if (!set.has(JSON.stringify({ name: srv[i].data.target, type: 'A', QU: false }))) {
         set.add(JSON.stringify({ name: srv[i].data.target, type: 'A', QU: false }))
-        // dnssdQ.push({ name: srv[i].data.target, type: 'A', QU: false })
+        dnssdQ.push({ name: srv[i].data.target, type: 'A', QU: false })
       }
     }
   }
@@ -79,8 +81,9 @@ mDNS.on('response', function (packet) {
       if (value.includes('url=')) { url = value.split('url=')[1] }
       // console.log(ipv4)
     }
+    console.log(`http://${ipv4}:${port}/model`)
     request(`http://${ipv4}:${port}/model`, function (_error, response, body) {
-      // console.log('body:', body)
+       console.log('body:', body)
     })
   } catch (_err) {
   }
@@ -88,7 +91,7 @@ mDNS.on('response', function (packet) {
 })
 saveLog()
 dnssdQ.push({ name: '_services._dns-sd._udp.local', type: 'PTR', QU: true })
-dnssdA.push({ name: '_tv.*.local', type: 'TXT', data: ['exp = key === "_light1"'] })
+dnssdA.push({ name: '_tv.*.local', type: 'TXT', data: [`exp = ${unfi.decode('values.size > 30')}`] })
 mDNS.query({ questions: dnssdQ, additionals: dnssdA })
 setInterval(() => {
   if ((timeup + 500) < now()) {
@@ -96,4 +99,4 @@ setInterval(() => {
     console.log(count)
     process.exit()
   }
-}, 100)
+}, 400)
